d20fd9c857c76076d49bd14dca249a0ac07dadda
erxonxi <rubenruizpedreira@gmail.com>
2021-08-20 10:55:40 +0200

    fix landing

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -9,13 +9,14 @@ const Landing = () => {
             <h1 className={'text-orange display-xl'}>
                 Full Stack Junior Developer
             </h1>
-            <p className={'font-24'}>
+            <p className={'font-20'}>
                 Soy un desarrollador autodidacta de España. Actualmente trabajo
                 en Nemon Intelligence In Business manteniendo y mejorando un
                 E.R.P y CRM para comercializadoras electricas.
             </p>
-            <p className={'font-28'}>
-                Puedes echar un vistazo a mi trabajo ⚡️ Espero que te guste 🙌
+            <p className={'font-20'}>
+                Te recomiendo revisar algunos de mis proyectos. Espero que te
+                guste 🙌
             </p>
         </div>
     );
