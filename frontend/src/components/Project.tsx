929a3c55d6a270591a6546797eb36f92e959fe33
erxonxi <rubenruizpedreira@gmail.com>
2021-08-20 16:38:43 +0200

    componetiando carta de proyectos y creando nuevo proyecto

---
+++ frontend/src/components/Project.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+const Project = ({
+    title = '',
+    description = '',
+    url_github = '',
+    url_demo = '',
+    img = '',
+    img_alt = '',
+}) => {
+    return (
+        <div className={'card'}>
+            <h2>{title}</h2>
+            <img src={img} alt={img_alt} />
+            <p>{description}</p>
+            <div className={'container-full grid col-2 sm-col-1'}>
+                <a className={'btn'} href={url_demo}>
+                    VER DEMO
+                </a>
+                <a className={'btn'} href={url_github}>
+                    <FaGithub className={'mr-12'} />
+                    VER CODIGO
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Project;
--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FaGithub, FaSearchengin } from 'react-icons/fa';
-import { FiSettings } from 'react-icons/fi';
+import { FaGithub } from 'react-icons/fa';
+import Project from './Project';
 
 const Projects = () => {
     return (
@@ -14,27 +14,26 @@ const Projects = () => {
                     Podras ver el codigo en mi Github.
                 </p>
                 <div className={'grid col-2 md-col-1 py-40'}>
-                    <div className={'card'}>
-                        <h2>Web Personal</h2>
-                        <img src="img/portafolio_img.png" alt="" />
-                        <p>
-                            Siempre quise desarollar una web para mostrar mis
-                            proyectos. Ese dia llego y puedes ver el codigo y la
-                            evolucion aqui.
-                        </p>
-                        <div className={'container-full grid col-2 sm-col-1'}>
-                            <a className={'btn'} href={'/'}>
-                                VER DEMO
-                            </a>
-                            <a
-                                className={'btn'}
-                                href={'https://github.com/erxonxi/portafolio'}
-                            >
-                                <FaGithub className={'mr-12'} />
-                                VER CODIGO
-                            </a>
-                        </div>
-                    </div>
+                    <Project
+                        title={'Web Personal'}
+                        description={
+                            'Siempre quise desarollar una web para mostrar mis proyectos. Ese dia llego y puedes ver el codigo y la evolucion aqui.'
+                        }
+                        img={'img/portafolio_img.png'}
+                        img_alt={'Portafolio'}
+                        url_demo={'/'}
+                        url_github={'https://github.com/erxonxi/portafolio'}
+                    />
+                    <Project
+                        title={'Giff App'}
+                        description={
+                            'Mi primer proyecto con ReactJS, aprendiendo ha hacer peticiones a una API.'
+                        }
+                        img={'img/project_giff_app.png'}
+                        img_alt={'Giff App'}
+                        url_demo={'https://giff-app.rubenruizpedreira.es'}
+                        url_github={'https://github.com/erxonxi/gif-app'}
+                    />
                     <div className={'card'}>
                         <h1 className={'text-center'}>
                             Working in deploy more apps...
