ddbe43fc9abc8c3b64d6a979d1f257c2b19ba7ed
erxonxi <rubenruizpedreira@gmail.com>
2021-08-24 12:44:28 +0200

    loadingpage and docker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Experience from './components/Experience';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
+import LoadingPage from './components/LoadingPage/LoadingPage';
 import NavBar from './components/NavBar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 const App = () => {
+    const [loadingPage, setLoadingPage] = useState(true);
+
+    if (loadingPage) {
+        setTimeout(() => {
+            setLoadingPage(false);
+        }, 3000);
+        return <LoadingPage />;
+    }
+
     return (
         <>
             <header>
---
+++ frontend/src/components/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './loading-page.css';
+
+const LoadingPage = () => {
+    return (
+        <section className={''}>
+            <div className={'loading loading05'}>
+                <span className={'title-gradiant'}>R</span>
+                <span className={'title-gradiant'}>R</span>
+            </div>
+        </section>
+    );
+};
+
+export default LoadingPage;
